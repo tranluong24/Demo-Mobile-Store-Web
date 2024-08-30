4b21ca0c2e6c04049b4e34143eefd1dc759aee94
=tranluong <gohan244123@gmail.com>
2024-08-31 05:32:38 +0700

    init website

---
+++ script.js
@@ -0,0 +1,25 @@
+// Xử lý sự kiện thêm vào giỏ hàng
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Đã thêm sản phẩm vào giỏ hàng!');
+    });
+});
+
+// Xử lý form liên hệ
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const name = contactForm.name.value;
+    const email = contactForm.email.value;
+    const message = contactForm.message.value;
+
+    if(name && email && message) {
+        alert('Cảm ơn bạn đã liên hệ, chúng tôi sẽ phản hồi sớm nhất!');
+        contactForm.reset();
+    } else {
+        alert('Vui lòng điền đầy đủ thông tin.');
+    }
+});
